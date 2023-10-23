b547ca2654eda058cd7acdaffb4c74360e536577
Name <email@exaple.ru>
2023-10-23 12:21:25 +0300

    Регистрация.

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace SocialNetwork
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System.Threading.Channels;
+
+namespace SocialNetwork
 {
-    internal class Program
+    public class Program
     {
+        public static UserService userService = new UserService();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Добро пожаловать в социальную сеть.");
+            while (true)
+            {
+                Console.WriteLine("Для регистрации пользователя введите имя:");
+
+                string firstName = Console.ReadLine();
+
+                Console.Write("Фамилия:");
+
+                string lastName = Console.ReadLine();
+
+                Console.Write("пароль:");
+
+                string password = Console.ReadLine();
+
+                Console.Write("почтовый адрес:");
+
+                string email = Console.ReadLine();
+
+                UserRegistrationData registrationData = new UserRegistrationData()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userService.Register(registrationData);
+                    Console.WriteLine("Регистрация произошла успешно!");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Введите корректное значение.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Произошла ошибка при регистрации.");
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.ReadKey();
+            }
         }
     }
 }
