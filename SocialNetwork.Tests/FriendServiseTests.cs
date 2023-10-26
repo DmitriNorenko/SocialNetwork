145165e04dbc1d86774e9eccbc112f61885d7134
Name <email@exaple.ru>
2023-10-26 11:44:49 +0300

    Nunit test

---
+++ SocialNetwork.Tests/FriendServiseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace SocialNetwork.Tests
+{
+    [TestFixture]
+    public class TestEmailFriend
+    {
+        [Test]
+        public void AddToFriend_MustReturnNullException()
+        {
+            var friendServise = new FriendServise();
+            var dataAddFriends = new DataAddFriend { RecipientEmail = "bob@mail.ru", user_id = 1};
+           Assert.Throws<Exception>(() => friendServise.AddFriend(dataAddFriends));
+        }
+    }
+}
